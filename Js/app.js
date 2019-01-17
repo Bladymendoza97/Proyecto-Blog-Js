ddf41fc65b1b570bf20b7c8feb3d75b5a5bdd700
BladyMendoza <tobysiezar@gmail.com>
2019-01-17 12:34:23 -0600

    Articulos Dinamicos, LocalStorage

--- Js/app.js
+++ Js/app.js
@@ -13,48 +13,45 @@ $('#toggle').click(function(){
 /* 1.6 LISTADO DINAMICO ARTICULOS */
 
 
-$(document).ready(function(){
+$('#publicar').submit(function(){
 
  var post = [
-   {
-     title:'Angular Cli',
-     date: 'Publicado el ' + moment().date() + ' de ' + moment().format('MMMM YYYY'),//libreria moment js
-     content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, facilis labore natus consectetur autem sit omnis dolor maxime nulla laudantium itaque sed ipsa eos iusto sint commodi quis asperiores tempore laboriosam assumenda dolorem id. Hic provident quaerat minima, excepturi soluta culpa dolore ex harum illum, corrupti libero recusandae molestias fugit atque, veritatis sequi ut minus nesciunt doloribus? Culpa quam dolore quas reiciendis ratione! Blanditiis facilis quis quia laboriosam. Facilis molestias consectetur consequuntur accusamus est adipisci ducimus voluptate eos, nobis quam natus doloremque. Molestiae, molestias omnis?'
+    
 
-   },
-   {
-    title:'Node Js',
-    date: 'Publicado el ' + moment().date() + ' de ' + moment().format('MMMM YYYY'), //libreria moment js
-    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, facilis labore natus consectetur autem sit omnis dolor maxime nulla laudantium itaque sed ipsa eos iusto sint commodi quis asperiores tempore laboriosam assumenda dolorem id. Hic provident quaerat minima, excepturi soluta culpa dolore ex harum illum, corrupti libero recusandae molestias fugit atque, veritatis sequi ut minus nesciunt doloribus? Culpa quam dolore quas reiciendis ratione! Blanditiis facilis quis quia laboriosam. Facilis molestias consectetur consequuntur accusamus est adipisci ducimus voluptate eos, nobis quam natus doloremque. Molestiae, molestias omnis?'
-
-  },
   {
-    title:'Ionic 2 y 3',
-    date: 'Publicado el ' + moment().date() + ' de ' + moment().format('MMMM YYYY'), //libreria moment js     
-    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, facilis labore natus consectetur autem sit omnis dolor maxime nulla laudantium itaque sed ipsa eos iusto sint commodi quis asperiores tempore laboriosam assumenda dolorem id. Hic provident quaerat minima, excepturi soluta culpa dolore ex harum illum, corrupti libero recusandae molestias fugit atque, veritatis sequi ut minus nesciunt doloribus? Culpa quam dolore quas reiciendis ratione! Blanditiis facilis quis quia laboriosam. Facilis molestias consectetur consequuntur accusamus est adipisci ducimus voluptate eos, nobis quam natus doloremque. Molestiae, molestias omnis?'
+    title:$('input[name="titulo"]').val(),
+    date: 'Publicado el ' + moment().date() + ' de ' + moment().format('MMMM YYYY') + '<br>' + 'a las ' + moment().format('h:mm a') , //libreria moment js
+    content:$('textarea[name="contenido"]').val()  
 
   },
-  {
-    title:'Laravel',
-    date: 'Publicado el ' + moment().date() + ' de ' + moment().format('MMMM YYYY'), //libreria moment js
-    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, facilis labore natus consectetur autem sit omnis dolor maxime nulla laudantium itaque sed ipsa eos iusto sint commodi quis asperiores tempore laboriosam assumenda dolorem id. Hic provident quaerat minima, excepturi soluta culpa dolore ex harum illum, corrupti libero recusandae molestias fugit atque, veritatis sequi ut minus nesciunt doloribus? Culpa quam dolore quas reiciendis ratione! Blanditiis facilis quis quia laboriosam. Facilis molestias consectetur consequuntur accusamus est adipisci ducimus voluptate eos, nobis quam natus doloremque. Molestiae, molestias omnis?'
 
-  },
+ 
  ];
 
-  post.forEach((item, index) => {
-    var post = `
-              <section class="post">
-                  <h2>${item.title}</h2>
-                  <small class="fecha">${item.date}</small>
-                  <p>${item.content}</p>
-                  <a href="#" class="button-more">Leer más</a>
-              </section>
-
-          `;     
-
-          $('#posts').append(post);
-  });
+ console.log(post[0]);
  
+ 
+ post.forEach((item, index) => {
+   var post = `
+   <section class="post">
+   <h2>${item.title}</h2>
+   <small class="fecha">${item.date}</small>
+   <p>${item.content}</p>
+   <a href="#" class="button-more">Leer más</a>
+   </section>
+   
+   `;     
+   
+   $('#posts').append(post);//Muestra los articulos en el body
+   
+  });
   
+  /*Almacena la informacion de los articulos en el LocalStorage */
+  // localStorage.setItem(post[0].title, JSON.stringify(post[0]));  
+
+  $('input[type="text"]').val('');//Borra los campos una vez se ha dado click en el boton
+  $('textarea[type="text"]').val('');//Borra los campos una vez se ha dado click en el boton
+
+ 
+  return false;
 });
